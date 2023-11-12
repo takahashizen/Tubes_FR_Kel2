33067c9b79e9d627260896830e5b38d8e122f074
afdinal <afdinalbtb16@gmail.com>
2023-11-12 10:06:46 +0700

    sizing the window

--- GUI/app.py
+++ GUI/app.py
@@ -2,7 +2,9 @@ import ttkbootstrap as ttk;
 import tkinter as tk;
 
 window = tk.Tk();
-
+width = window.winfo_screenwidth();
+height = window.winfo_screenheight();
+window.geometry(f"{width}x{height}");
 
 ############################
 if __name__ == '__main__':
